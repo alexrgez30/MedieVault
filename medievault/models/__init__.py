2f626ae69b6d6f14b4ee3044ba8abc7b30b2ea59
Alejandro <alejandrord3006@gmail.com>
2026-03-27 11:34:22 +0100

    feat(models): expose all domain models from package __init__

---
+++ medievault/models/__init__.py
@@ -0,0 +1,20 @@
+from medievault.models.books import Author, Book, ReadStatus
+from medievault.models.games import Game, Platform, PlayStatus
+from medievault.models.music import Album, Artist, Track
+from medievault.models.screen import Episode, MediaStatus, Movie, Series
+
+__all__ = [
+    "Artist",
+    "Album",
+    "Track",
+    "Movie",
+    "Series",
+    "Episode",
+    "MediaStatus",
+    "Author",
+    "Book",
+    "ReadStatus",
+    "Game",
+    "Platform",
+    "PlayStatus",
+]
